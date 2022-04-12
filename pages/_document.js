bdd8fc26bcb6aeb996c3e26e13c817a639b2f0ed
Peter Hagen <pha@hey.com>
2022-04-12 22:25:22 +0200

    Add more icon sizes

---
+++ pages/_document.js
@@ -0,0 +1,40 @@
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+class Document extends NextDocument {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default Document;
