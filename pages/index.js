0367b9d2b0c5b7e973d2dc0996c17a4330184e28
Peter Hagen <pha@hey.com>
2022-04-13 16:50:00 +0200

    Link to example pages

--- pages/index.js
+++ pages/index.js
@@ -99,7 +99,7 @@ export default function Home() {
         <div className="text-xl">For the love of internet articles.</div>
         <div className="mt-7 flex flex-wrap justify-center gap-5">
           {Array.from(Array(21).keys()).map((i) => (
-            <ExamplePage key={i} src={`/media/pages/screenshot_${i}.png`} />
+            <ExamplePage key={i} index={i} />
           ))}
         </div>
       </div>
@@ -111,10 +111,46 @@ export default function Home() {
   );
 }
 
-function ExamplePage({ src }) {
+const exampleUrls = [
+  "http://www.paulgraham.com/ds.html",
+  "https://kk.org/thetechnium/1000-true-fans/ ",
+  "https://www.joelonsoftware.com/2001/04/21/dont-let-architecture-astronauts-scare-you/",
+  "https://waitbutwhy.com/2015/01/artificial-intelligence-revolution-1.html",
+  "https://www.tesla.com/blog/secret-tesla-motors-master-plan-just-between-you-and-me",
+  "https://arstechnica.com/science/2014/09/the-little-known-soviet-mission-to-rescue-a-dead-space-station/ ",
+  "https://blog.samaltman.com/the-days-are-long-but-the-decades-are-short ",
+  "https://www.theguardian.com/lifeandstyle/2014/jul/19/change-your-life-sit-down-and-think ",
+  "https://sive.rs/kimo",
+  "http://www.aaronsw.com/weblog/rewritingreddit ",
+  "https://www.fastcompany.com/28121/they-write-right-stuff ",
+  "http://antirez.com/news/61 ",
+  "https://www.economist.com/open-future/2018/11/26/ai-thinks-like-a-corporation-and-thats-worrying ",
+  "https://www.bloomberg.com/news/features/2018-05-03/the-gambler-who-cracked-the-horse-racing-code ",
+  "https://www.kalzumeus.com/2011/10/28/dont-call-yourself-a-programmer/ ",
+  "https://martinfowler.com/bliki/MonolithFirst.html ",
+  "https://www.raptitude.com/2016/12/five-things-you-notice-when-you-quit-the-news/ ",
+  "https://www.newyorker.com/tech/annals-of-technology/walking-helps-us-think ",
+  "https://slatestarcodex.com/2014/09/30/i-can-tolerate-anything-except-the-outgroup/",
+  "https://www.nytimes.com/2012/07/15/fashion/the-challenge-of-making-friends-as-an-adult.html ",
+  "https://www.theatlantic.com/magazine/archive/1982/02/have-you-ever-tried-to-sell-a-diamond/304575/ ",
+];
+
+function ExamplePage({ index }) {
   return (
-    <div className="w-48 rounded-lg shadow-lg hover:shadow-2xl">
-      <Image className="rounded-lg" src={src} width={1360} height={1600} />
-    </div>
+    <a
+      className={
+        "w-48 flex rounded-lg shadow-lg hover:shadow-2xl transition-all " +
+        (index % 3 === 0 ? "hover:-rotate-1" : "hover:rotate-1")
+      }
+      href={exampleUrls[index]}
+      target="_blank"
+    >
+      <Image
+        className="rounded-lg"
+        src={`/media/pages/screenshot_${index}.png`}
+        width={1360}
+        height={1600}
+      />
+    </a>
   );
 }
