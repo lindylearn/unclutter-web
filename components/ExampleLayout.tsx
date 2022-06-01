86b2ed2f945380b5d5b777dca92e9c8398decc12
Peter Hagen <pha@hey.com>
2022-06-01 16:29:38 +0200

    Fix mobile style for new layout

--- components/ExampleLayout.tsx
+++ components/ExampleLayout.tsx
@@ -25,20 +25,20 @@ export default function ExampleLayout({
     return (
         <div
             className={
-                "w-5/6 flex flex-col md:flex-row gap-3 md:gap-10 justify-start " +
+                "flex flex-col md:flex-row gap-3 md:gap-5 lg:gap-10 justify-start items-start " +
                 (!inView && defaultVisible ? "opacity-20" : "") +
                 (!inView && !defaultVisible ? "opacity-0" : "") +
                 (inView && !defaultVisible ? "xl:animate-slidein" : "")
             }
             ref={ref}
         >
-            <div className="w-full md:max-w-lg flex-shrink-0 relative rounded-xl overflow-hidden shadow-xl desktop:hover:cursor-pointer desktop:hover:shadow-2xl">
+            <div className="w-full md:max-w-sm lg:max-w-lg flex-shrink-0 relative rounded-xl overflow-hidden shadow-xl desktop:hover:cursor-pointer desktop:hover:shadow-2xl">
                 <ExampleLayoutContext.Provider value={{ inView }}>
                     {children}
                 </ExampleLayoutContext.Provider>
             </div>
 
-            <div className="flex-shrink-0 flex flex-col md:mt-5 gap-1 md:gap-3 items-start">
+            <div className="max-w-full  flex flex-col md:mt-5 gap-1 md:gap-3 items-start">
                 <div className="text-xl md:text-2xl max-w-2xl">
                     <b className="font-bold md:text-[26px]">{boldTitle}</b>{" "}
                     {title}
