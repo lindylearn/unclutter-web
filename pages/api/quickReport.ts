f786b338865253f490325455a84a9a760f2c9b17
Peter Hagen <pha@hey.com>
2022-10-13 16:33:44 +0200

    Fix api call

--- pages/api/quickReport.ts
+++ pages/api/quickReport.ts
@@ -1,6 +1,15 @@
 export default async function handler(req, res) {
-    const data = JSON.parse(req.body);
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    if (req.method === "OPTIONS") {
+        res.setHeader(
+            "Access-Control-Allow-Methods",
+            "PUT, POST, PATCH, DELETE, GET"
+        );
+        return res.status(200).json({});
+    }
 
+    const data = req.body;
     const response = await fetch(
         `https://api.github.com/repos/lindylearn/unclutter/issues`,
         {
@@ -9,10 +18,8 @@ export default async function handler(req, res) {
                 title: `Quick report`,
                 body: `${data.message}
 
-Current url: \`${data.url}\`
 Unclutter version: \`${data.unclutterVersion}\`
-Browser type: \`${data.browserType}\`
-User Agent: \`${data.userAgent}\``,
+Browser type: \`${data.browserType}\`,
                 labels: [],
             }),
             headers: {
@@ -22,7 +29,6 @@ User Agent: \`${data.userAgent}\``,
             },
         }
     );
-    console.log(response);
     const json = await response.json();
 
     res.status(200).send(json.url);
