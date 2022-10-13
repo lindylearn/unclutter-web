991eb77ff5455a9fd1f7cda33c9fe218ae9320da
Peter Hagen <pha@hey.com>
2022-10-13 16:14:17 +0200

    Return issue url

--- pages/api/quickReport.ts
+++ pages/api/quickReport.ts
@@ -1,22 +1,26 @@
 export default async function handler(req, res) {
     const data = JSON.parse(req.body);
 
-    await fetch(`https://api.github.com/repos/lindylearn/unclutter/issues`, {
-        method: "POST",
-        body: JSON.stringify({
-            title: `Quick report`,
-            body: `${data.message}
+    const response = await fetch(
+        `https://api.github.com/repos/lindylearn/unclutter/issues`,
+        {
+            method: "POST",
+            body: JSON.stringify({
+                title: `Quick report`,
+                body: `${data.message}
 
 Current url: \`${data.url}\`
 Unclutter version: \`${data.unclutterVersion}\`
 Browser type: \`${data.browserType}\`
 User Agent: \`${data.userAgent}\``,
-            labels: [],
-        }),
-        headers: {
-            Authorization: `token ${process.env.GITHUB_BOT_TOKEN}`,
-        },
-    });
+                labels: [],
+            }),
+            headers: {
+                Authorization: `token ${process.env.GITHUB_BOT_TOKEN}`,
+            },
+        }
+    );
+    const json = await response.json();
 
-    res.status(200).send("ok");
+    res.status(200).send(json.url);
 }
