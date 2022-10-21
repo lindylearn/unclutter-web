3b437ef39fc2e74acd38a222ec223fb62b0765bd
Peter Hagen <pha@hey.com>
2022-10-21 15:32:37 +0200

    Update ogimage

--- components/Head.tsx
+++ components/Head.tsx
@@ -13,11 +13,11 @@ export default function SiteHead({ title, description }) {
 
             <meta
                 property="og:image"
-                content="https://unclutter.lindylearn.io/ogimagex.png"
+                content="https://unclutter.lindylearn.io/ogimagey.png"
             />
             <meta
                 name="twitter:image"
-                content="https://unclutter.lindylearn.io/ogimagex.png"
+                content="https://unclutter.lindylearn.io/ogimagey.png"
             />
 
             <meta name="twitter:card" content="summary_large_image" />
