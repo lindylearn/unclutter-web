0edd011d631e156d1286362bb3d9ab4a9dfdf303
Peter Hagen <pha@hey.com>
2022-11-04 20:07:24 +0100

    Add /v2 redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const { withPlausibleProxy } = require("next-plausible");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: "/v2",
+                destination: "/",
+                permanent: true,
+            },
+        ];
+    },
 };
 
 module.exports = withPlausibleProxy()(nextConfig);
